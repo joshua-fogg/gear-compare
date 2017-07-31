f9810b2162e3340d9c01bbe1a34fb6b1ea779e90
Joshua Fogg <joshuafoggnz@gmail.com>
2017-07-31 18:27:43 +1200

    added compare component

---
+++ client/Compare.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+
+    return (
+        <div className='coppare'>
+            <div id='compare-filters'>
+                filters to apply
+            </div>
+            <div id='#'>
+            Panel for listing gear
+            </div>
+    </div >
+    )
+}
+export default Home
