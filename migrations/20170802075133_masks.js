39c8f0bc53933b018d6018a8b8d47b73af3da287
joshua-fogg <joshuafoggnz@gmail.com>
2017-08-02 18:11:39 +1200

    edit schema

--- migrations/20170802075133_masks.js
+++ migrations/20170802075133_masks.js
@@ -1,8 +1,11 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTableIfNotExists('masks', function(table) {
-        table.string('model_id').primary()
-        table.string('lense_type')
-        table.string('mask_profile')
+        table.string('Model_ID').primary()
+        table.string('Lense')
+        table.string('Frame')
+        table.string('Skirt')
+        table.string('Mask_Volume')
+        table.string('')
     })
 };
 
