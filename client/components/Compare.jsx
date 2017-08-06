0ed0b04bad9f4dbcf8ab4ca9941ca5e812e37ba1
sethman6 <joshuafoggnz@gmail.com>
2017-08-06 15:45:56 +1200

    Building Compare; sudo + assess url + map()

--- client/components/Compare.jsx
+++ client/components/Compare.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import db from '/database/db'
+import catalogue from '/database/catalogue'
+
 const Compare = (props) => {
-  const category = rops.match.params.match.category
+  const category = props.match.params.match.category
   const firstProduct = props.params.match.id1
   const secondProduct = props.params.match.id2
   const thirdProduct = props.params.match.id3
+
+
+  function assessCategory(category) {
+    if (isNaN(category) === false) { //no category, = number
+      //render all categories
+    } else {//has category
+      catalogue.data.category.map((itemInfo) => {
+        return (
+          <div className='category card' key={itemInfo.id} >
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="http://via.placeholder.com/300x300" alt="Image" />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">{category.name}</p>
+                </div>
+              </div>
+              <div class="content">
+                {category.summary}
+              </div>
+            </div>
+          </div>
+        )
+      })
+      //map category
+    }
+  }
+  // based on category, must pull different data from db and set it to props / state.
+  // must map out db data into boxes.
+  //add checkbox / select state for comparing items
+  //have function to show only those items that are viewed.
 
   return (
     <div className='compare'>
---
+++ public/database/catalogue.js
@@ -0,0 +1,14 @@
+const data = {
+    "fins": [
+        { "id": "1", "brand": "Apollo", "model": "Pro", "make": "Bio Fin", "heel_type": "Spring Strap", "fin_type": "Split Fin", "class": null, "colours": "Black" },
+        { "id": "2", "brand": "Apollo", "model": "Ranger", "make": "Bio Fin", "heel_type": "Spring Strap", "fin_type": "Split Fin", "class": null, "colours": "Black" },
+        { "id": "3", "brand": "Oceanic", "model": "Power Thrust", "make": "Viper", "heel_type": "Quick Release", "fin_type": "Blade", "class": "Innvoative", "colours": "Black, Blue, Neon Yellow, Red, White/pink, White/Sea Blue" },
+        { "id": "4", "brand": "Mares", "model": null, "make": "X-Stream", "heel_type": "Quick Release", "fin_type": "Blade", "class": "Innovative", "colours": "Black, Blue, Pink, Red, White, Yellow" }
+    ],
+    "masks": [
+        { "id": 1, "model_id": "1azx-Aql", "lense": "Single", "frame": "Plastic", "lense_material": "Tempered", "skirt": "Signle", "mask_volume": "Medium", "buckels": "Cervical joint" },
+        { "id": 2, "model_id": "2azy-mare", "lense": "Double", "frame": "Plastic", "lense_material": "Normal", "skirt": "Single", "mask_volume": "Low", "buckels": "Hard Buckel" },
+        { "id": 3, "model_id": "23a-apex", "lense": "Single", "frame": "Metal", "lense_material": "Plastic", "skirt": "Single", "mask_volume": "High", "buckels": "Cervical" }
+    ]
+
+}
