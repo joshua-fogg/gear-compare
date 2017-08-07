4d123ad52407bdd015eb11e9d72f361f26f6eee5
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-08 11:02:07 +1200

    trying to normalise data

---
+++ client/newdataformat.js
@@ -0,0 +1,32 @@
+const data = {
+    "item": [{
+        id: 1,
+        model_id: "", //Cat11l
+        brand: "", // Apollo, Mares, Aqualung, Faber, Catalina
+        model: "", // 11LAl
+        features: {
+            color: '',
+
+        },
+
+        make: "Bio Fin",
+        heel_type: "Spring Strap",
+        fin_type: "Split Fin",
+        class: null,
+        colours: "Black"
+    }],
+    finsSpecific: {
+        heel_type: "Spring Strap",
+        fin_type: "Split Fin",
+        class: null,
+    },
+    tankSpecific: {
+        regulatoryMarkings
+    }
+}
+
+// brand = maker / brand
+// model = model specific name 
+// Uid =  anyid type from catalogue
+//
+export default data
