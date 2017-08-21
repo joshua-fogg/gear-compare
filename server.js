8af1672834823b9a87e6214af9a1c9d112923cda
Joshua <joshuafoggnz@gmail.com>
2017-08-22 10:56:12 +1200

    tidy server

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var path = require('path')
 var config = require('./knexfile').development
 var db = require('knex')(config)
 
-
-
 var PORT = process.env.PORT || 3000
 server.use(express.static(path.join(__dirname, 'public')));
 
