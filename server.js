7fc62e3b9a67859e2a91527bf4b242bc103de478
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-21 16:45:40 +1200

    lingting

--- server.js
+++ server.js
@@ -5,27 +5,23 @@ var path = require('path')
 var config = require('./knexfile').development
 var db = require('knex')(config)
 
-
-
 var PORT = process.env.PORT || 3000
 server.use(express.static(path.join(__dirname, 'public')));
 
-
-server.listen(PORT, function() {
-    console.log('Listening on port', PORT)
+server.listen(PORT, function () {
+  console.log('Listening on port', PORT)
 })
 
-
 server.get('/finData', (req, res) => {
-    db('fins')
-        .then((fins) => {
-            res.json(fins)
-        })
+  db('fins')
+    .then((fins) => {
+      res.json(fins)
+    })
 })
 
 server.get('/maskData', (req, res) => {
-    db('masks')
-        .then((fins) => {
-            res.json(fins)
-        })
+  db('masks')
+    .then((fins) => {
+      res.json(fins)
+    })
 })
