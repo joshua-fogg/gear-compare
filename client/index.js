77fdf4b23fe4c77ae1dde896db2363adad08e5cb
Joshua <joshuafoggnz@gmail.com>
2017-08-22 17:36:09 +1200

    Redux psudo, no install

--- client/index.js
+++ client/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+// import [createStore] from 'redux'
+// import { Provider } from 'react-redux'
 import App from './App'
 
+
+//store = createStore( state = initialstate, action){
+// return state
+// }
+
+// const store = createStore(reducer)
+// console.log(state)
+
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render( <
-        App / > ,
+    ReactDOM.render(
+        // <Provider>
+        <
+        App / >
+        // </Provider>
+        ,
         document.getElementById('root')
     )
 })
