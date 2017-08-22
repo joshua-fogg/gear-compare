0a84abfc19f3442667c917f2880dc1ab549eb00d
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-08-23 08:32:45 +1200

    indent

--- seeds/fins.js
+++ seeds/fins.js
@@ -1,15 +1,15 @@
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('fins').del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('fins').insert([
-                { brand: 'Apollo', make: 'Bio Fin', prod_number: '1', model: 'Pro', heel_type: 'Spring Strap', fin_type: 'Split Fin', class: '', colours: 'Black' },
-                { brand: 'Apollo', make: 'Bio Fin', prod_number: '2', model: 'Ranger', heel_type: 'Spring Strap', fin_type: 'Split Fin', class: '', colours: 'Black' },
-                { brand: 'Oceanic', make: 'Viper', prod_number: '3', model: 'Power Thrust', heel_type: 'Quick Release', fin_type: 'Blade', class: 'Innvoative', colours: 'Black, Blue, Neon Yellow, Red, White/pink, White/Sea Blue' },
-                { brand: 'Mares', make: 'X-Stream', prod_number: '', model: '', heel_type: 'Quick Release', fin_type: 'Blade', class: 'Innovative', colours: 'Black, Blue, Pink, Red, White, Yellow' },
-            ]);
-        });
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('fins').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('fins').insert([
+        { brand: 'Apollo', make: 'Bio Fin', prod_number: '1', model: 'Pro', heel_type: 'Spring Strap', fin_type: 'Split Fin', class: '', colours: 'Black' },
+        { brand: 'Apollo', make: 'Bio Fin', prod_number: '2', model: 'Ranger', heel_type: 'Spring Strap', fin_type: 'Split Fin', class: '', colours: 'Black' },
+        { brand: 'Oceanic', make: 'Viper', prod_number: '3', model: 'Power Thrust', heel_type: 'Quick Release', fin_type: 'Blade', class: 'Innvoative', colours: 'Black, Blue, Neon Yellow, Red, White/pink, White/Sea Blue' },
+        { brand: 'Mares', make: 'X-Stream', prod_number: '', model: '', heel_type: 'Quick Release', fin_type: 'Blade', class: 'Innovative', colours: 'Black, Blue, Pink, Red, White, Yellow' },
+      ]);
+    });
 };
 
 //Brand = Apollo, Oceanic, Aqualung, Mares
--- seeds/masks.js
+++ seeds/masks.js
@@ -1,14 +1,14 @@
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('masks').del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('masks').insert([
-                { model_id: '1azx-Aql', lense: 'Single', frame: 'Plastic', lense_material: 'Tempered', Skirt: 'Signle', mask_volume: 'Medium', buckels: 'Cervical joint' },
-                { model_id: '2azy-mare', lense: 'Double', frame: 'Plastic', lense_material: 'Normal', Skirt: 'Single', mask_volume: 'Low', buckels: 'Hard Buckel' },
-                { model_id: '23a-apex', lense: 'Single', frame: 'Metal', lense_material: 'Plastic', Skirt: 'Single', mask_volume: 'High', buckels: 'Cervical' },
-            ]);
-        });
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('masks').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('masks').insert([
+        { model_id: '1azx-Aql', lense: 'Single', frame: 'Plastic', lense_material: 'Tempered', Skirt: 'Signle', mask_volume: 'Medium', buckels: 'Cervical joint' },
+        { model_id: '2azy-mare', lense: 'Double', frame: 'Plastic', lense_material: 'Normal', Skirt: 'Single', mask_volume: 'Low', buckels: 'Hard Buckel' },
+        { model_id: '23a-apex', lense: 'Single', frame: 'Metal', lense_material: 'Plastic', Skirt: 'Single', mask_volume: 'High', buckels: 'Cervical' },
+      ]);
+    });
 };
 // 'Model_ID' = primary, Product Code
 // string('Lense') = Single, Double
