8c89eecabefb9889370e3887fa3986b7b62de86e
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-09-08 16:07:09 +1200

    removing dead code

--- client/components/Compare.jsx
+++ client/components/Compare.jsx
@@ -29,22 +29,7 @@ class Compare extends Component {
         ]
       })
     }
-    // let selectedEQ = [...this.state.selectedEquipment]
-    // console.log('EID:', EId, 'selectedEQ:', selectedEQ)
-    // if (selectedEQ.includes(EId)) {
-    //   let selectedEQ = selectedEQ.filter(id => EId !== id)
-    // } else {
-    //   selectedEQ.push(EId)
-    // }
-    // this.setState({ selectedEquipment: selectedEQ })
   }
-
-  // removeFromStore(EId) {
-  //   let equipmentList = [...this.state.selectedEquipment].filter((equipment) => {
-  //     return equipment != EId;
-  //   })
-  //   this.setState({selectedEquipment: equipmentList})
-  // }
 
   render() {
     const category = this.props.match.params.id
@@ -78,13 +63,3 @@ export default Compare
 //add ins: have buttons on bottom of page for equipment categories?
 //change ID'd to be unique / class specific => condense state to be 1 state => can be loadout
 //turn each state changer into a toggler function
-
-
-  // addFinToStore(fin) {
-  //   let selectedFins = [...this.state.selectedFins]
-  //   //'...' makes state immutable
-  //   selectedFins.push(fin)
-  //   // addes new id to array
-  //   this.setState({ selectedFins: selectedFins })
-  //   //sets new array to state
-  // }
