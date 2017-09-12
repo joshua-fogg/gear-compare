99a25f4c1110b9b7d5fb069b998ac88514f19f76
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-09-12 16:53:20 +1200

    finish product_features

--- migrations/3_product_features.js
+++ migrations/3_product_features.js
@@ -1,12 +1,12 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('product_features', function (table) {
     table.increments('id')
-    table.string('name')
-    table.string('description')
+    table.string('product_id')
+    table.string('feature_id')
   })
 }
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('product_features')
 }
