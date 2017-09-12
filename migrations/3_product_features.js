d934067ed8ad1490fd8a19af572d106b04d30fef
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-09-12 16:50:53 +1200

    remove old schema

--- migrations/3_product_features.js
+++ migrations/3_product_features.js
@@ -1,6 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTableIfNotExists('product_features', function (table) {
+    table.increments('id')
+    table.string('name')
+    table.string('description')
+  })
 }
 
 exports.down = function(knex, Promise) {
