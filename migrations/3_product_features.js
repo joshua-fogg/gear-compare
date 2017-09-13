44cb21f3df4ca4ff94f46bd52ca243573d6cfc40
Joshua-Fogg <joshuafoggnz@gmail.com>
2017-09-13 16:31:16 +1200

    joining product features to other tables

--- migrations/3_product_features.js
+++ migrations/3_product_features.js
@@ -2,8 +2,8 @@
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('product_features', function (table) {
     table.increments('id')
-    table.string('product_id')
-    table.string('feature_id')
+    table.string('product_id').references('products.product_number')
+    table.string('feature_id').references('features.id')
   })
 }
 
