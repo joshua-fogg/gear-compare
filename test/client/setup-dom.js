76cad2d79459109aae74b75cdd1bd0d3d746b4b1
Joshua <joshuafoggnz@gmail.com>
2017-09-20 11:17:34 +1200

    add comment

--- test/client/setup-dom.js
+++ test/client/setup-dom.js
@@ -4,6 +4,6 @@ const { JSDOM } = require('jsdom')
 
 const dom = new JSDOM('<body></body>')
 
-global.window = dom.window
+global.window = dom.window // Targetting global window = DOM
 global.document = dom.window.document
 global.navigator = dom.window.navigator
